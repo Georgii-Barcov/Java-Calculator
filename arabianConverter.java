d87b11d0a96c734df0fa13cb31052af42fca324c
Georgii-Barcov <141604282+Georgii-Barcov@users.noreply.github.com>
2023-08-10 17:21:34 +0300

    Update arabianConverter.java

--- arabianConverter.java
+++ arabianConverter.java
@@ -1,10 +1,9 @@
-package arabianConverter;
-
 import java.util.TreeMap;
 
 class Converter {
     TreeMap<Character, Integer> romanKeyMap = new TreeMap<>();
 
+    TreeMap<Integer, String> arabianKeyMap = new TreeMap<>();
 
     public Converter () {
         romanKeyMap.put('I', 1);
@@ -15,14 +14,55 @@ class Converter {
         romanKeyMap.put('D', 500);
         romanKeyMap.put('M', 1000);
 
+        arabianKeyMap.put(1000, "M");
+        arabianKeyMap.put(500, "D");
+        arabianKeyMap.put(100, "C");
+        arabianKeyMap.put(50, "L");
+        arabianKeyMap.put(10, "X");
+        arabianKeyMap.put(9, "IX");
+        arabianKeyMap.put(5, "V");
+        arabianKeyMap.put(4, "IV");
+        arabianKeyMap.put(1, "I");
+
 
 
 
     }
 
-    public boolean  isRoman(String number){
-        //"V" -> 'V'
+    public boolean isRoman(String number){
         return romanKeyMap.containsKey(number.charAt(0));
     }
-}
 
+    public String intToRoman(int number) {
+        String roman = "";
+        int arabianKey;
+        do {
+            arabianKey = arabianKeyMap.floorKey(number);
+            roman += arabianKeyMap.get(arabianKey);
+            number -= arabianKey;
+        } while (number != 0);
+        return roman;
+
+
+    }
+    //XV
+    public int romanToInt(String s) {
+        int end = s.length() - 1;
+        char[] arr = s.toCharArray();
+        int arabian;
+        int result = romanKeyMap.get(arr[end]);
+        for (int i = end - 1; i >= 0; i--) {
+            arabian = romanKeyMap.get(arr[i]);
+
+            if (arabian < romanKeyMap.get(arr[i + 1])) {
+                result -= arabian;
+            } else {
+                result += arabian;
+            }
+
+
+        }
+        return result;
+
+    }
+}
