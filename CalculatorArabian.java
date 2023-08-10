09edd8a1c42d4a65a066a7a2580b4b92513313d8
Georgii-Barcov <141604282+Georgii-Barcov@users.noreply.github.com>
2023-08-10 17:22:57 +0300

    Update CalculatorArabian.java

--- CalculatorArabian.java
+++ CalculatorArabian.java
@@ -1,63 +1,78 @@
-package CalculatorArabian;
-
-
-
-import arabian.Converter;
-import arabian.ConverterBuilder;
-
 import java.util.Scanner;
 
+import static java.lang.Integer.parseInt;
+
 public class CalculatorArabian {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //2+3
         //V-VII
-        Converter converter = new ConverterBuilder().createConverter();
+        Converter converter = new Converter();
         String[] actions = {"+", "-", "/", "*",};
         String[] regexActions = {"\\+", "-", "/", "\\*"};
         Scanner csn = new Scanner(System.in);
         System.out.print("Введите выражение: ");
         String exp = csn.nextLine();
         //Определяем арифметическое действие
-        int actionIndex=-1;
-        for (int i = 0; i <actions.length; i++ ) {
-            if(exp.contains(actions[i])) {
+        int actionIndex = -1;
+        for (int i = 0; i < actions.length; i++) {
+            if (exp.contains(actions[i])) {
                 actionIndex = i;
                 break;
             }
         }
-        //Если не нашли арифметического действия завершаем программу
+        //Если не нашли арифметического действия, завершаем программу
         if(actionIndex==-1){
             System.out.println("Некорректное выражение");
             return;
         }
-        //"2-4".split("\\+")-> {"2", "4"}
+        //Делим строчку по найденному арифметическому знаку
+
+
         String[] data = exp.split(regexActions[actionIndex]);
         //Определяем, находятся ли числа в одном формате (оба римские или оба арабские)
-        if(converter.isRoman(data[0]) == converter.isRoman(data[1])) {
-            int a, b;
+        if(converter.isRoman(data[0]) == converter.isRoman(data[1])){
+            int a,b;
+            //Определяем, римские ли это числа
+            boolean isRoman = converter.isRoman(data[0]);
+            if(isRoman){
+                //если римские, то конвертируем их в арабские
+                a = converter.romanToInt(data[0]);
+                b = converter.romanToInt(data[1]);
 
-            //Конвертируем арабские числа из строки в число
-            a = Integer.parseInt(data[0]);
-            b = Integer.parseInt(data[1]);
-
-            //Выполняем с числами арифметическое действие
-            int result = switch (actions[actionIndex]) {
-                case "+" -> a + b;
-                case "-" -> a - b;
-                case "*" -> a * b;
-                default -> a / b;
-            };
-            //Если числа были арабские, возвращаем результат в арабском числе
-            System.out.println(result);
-
+            }else{
+                //если арабские, конвертируем их из строки в число
+                a = Integer.parseInt(data[0]);
+                b = Integer.parseInt(data[1]);
+            }
+            //выполняем арифметическое действие
+            int result;
+            switch (actions[actionIndex]){
+                case "+":
+                    result = a+b;
+                    break;
+                case "-":
+                    result = a-b;
+                    break;
+                case "*":
+                    result = a*b;
+                    break;
+                default:
+                    result = a/b;
+                    break;
+            }
+            //15->XV
+            if(isRoman){
+                //если числа были римские, возвращаем результат в римском числе
+                System.out.println(converter.intToRoman(result));
+            }
+            else{
+                //если числа были арабские, возвращаем результат в арабском числе
+                System.out.println(result);
+            }
         }else{
             System.out.println("Числа должны быть в одном формате");
-
-
-
-
-
-
         }
+
+
     }
 }
